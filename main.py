30bdcd335ab024ec115a2cb0eabd6057027c79f9
Amar <gosaviamar681999@gmail.com>
2021-05-14 22:37:31 +0530

    Validation done to login page

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from tkinter import *   
+from tkinter import *  
+from tkinter import messagebox 
+
+# removing all grid elements from screen
+class Remove:
+     def __init__(self, master):
+          super(Remove, self).__init__()
+          self.master = master
+
+     def remove_all_widgets(self):
+          for widgets in self.master.winfo_children():  # this is used only for grid system.
+               widgets.grid_remove()
+        
+
 
 class MainWindow:
 
@@ -11,10 +24,11 @@ class MainWindow:
 
      def loginWindow(self):
           heading = Label(self.master, text="Vehicle Parking Management System", font="verdana 22 bold", bg=self.color)
-          heading.pack(pady=20)
+          heading.grid(row=0, column=0, padx=(300,0), pady=20)
 
+          
           self.loginFrame = LabelFrame(self.master, text="")
-          self.loginFrame.pack(padx=50, pady=50)
+          self.loginFrame.grid(row=1, column=0, padx=(300,0), pady=50)
 
           self.username_label = Label(self.loginFrame, text="Username", font=self.font, bg="#f2f2f2")
           self.username_label.grid(row=0, column=0, padx=30, pady=(40,20))
@@ -28,10 +42,28 @@ class MainWindow:
           self.password_entry = Entry(self.loginFrame, textvariable=self.__password, width=20, font="verdana 12")
           self.password_entry.grid(row=1, column=1, padx=(10,30), pady=20, ipady=3)
 
-          self.login_button = Button(self.loginFrame, text="Login", width="20", font="verdana 12 bold", bg="#00a5aa", fg="#fff", cursor="hand2")
+          self.login_button = Button(self.loginFrame, text="Login", width="20", font="verdana 12 bold", bg="#00a5aa", fg="#fff", cursor="hand2", command=lambda: self.authenticate())
           self.login_button.grid(row=2, column=0, columnspan=2, padx=30, pady=30)
 
           pass
+
+     def authenticate(self):
+          self.__username = self.username_entry.get()
+          self.__password = self.password_entry.get()
+
+          if len(self.__username) <= 0:
+               messagebox.showerror("Invalid Credentials", "Please enter the username", parent=self.master)
+               self.username_entry.delete(0, END)
+               self.username_entry.focus()
+               return
+          if len(self.__password) <= 0:
+               messagebox.showerror("Invalid Credentials", "Please enter the password", parent=self.master)
+               self.password_entry.delete(0, END)
+               self.password_entry.focus()
+               return
+          
+          print("Username", self.__username, "is logged in into the system..")
+          Remove.remove_all_widgets(self.loginFrame)
 
 
 if __name__ == '__main__':
